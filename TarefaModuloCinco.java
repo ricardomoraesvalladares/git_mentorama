56d90a63c1806db4ec7cb354a5f23554dae8e10f
ricardomoraesvalladares <52608799+ricardomoraesvalladares@users.noreply.github.com>
2022-03-07 12:43:44 -0300

    Add files via upload

---
+++ TarefaModuloCinco.java
@@ -0,0 +1,18 @@
+
+public class TarefaModuloCinco {
+
+    public static void main(String[] args) {
+        Integer numero = Integer.parseInt("6");
+        System.out.println(numero);
+        System.out.println(numero.getClass());        
+        
+        int num = numero * 2;
+        System.out.println(num);
+
+        String valor = "Exemplo de string";
+        System.out.println(valor);
+        System.out.println("String em maiusculo: " + valor.toUpperCase());
+    }
+
+
+}
